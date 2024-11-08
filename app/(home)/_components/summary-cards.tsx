68aa51fcf8f985f518b8cc0f50b4a2714c5e0460
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-11-08 15:13:17 -0300

    feat: add summary cards

---
+++ app/(home)/_components/summary-cards.tsx
@@ -0,0 +1,78 @@
+import { db } from "@/app/_lib/prisma";
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import { SummaryCard } from "./summary-card";
+
+interface SummaryCardsProps {
+  month: string;
+}
+
+export const SummaryCards = async ({ month }: SummaryCardsProps) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "DEPOSIT" },
+        _sum: { amount: true },
+      })
+    )?._sum.amount,
+  );
+  const investimentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "INVESTMENT" },
+        _sum: { amount: true },
+      })
+    )?._sum.amount,
+  );
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "EXPENSE" },
+        _sum: { amount: true },
+      })
+    )?._sum.amount,
+  );
+  const balance = depositsTotal - investimentsTotal - expensesTotal;
+
+  return (
+    <div className="space-y-6">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investimentsTotal}
+        />
+
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesas"
+          amount={expensesTotal}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ app/(home)/page.tsx
@@ -0,0 +1,47 @@
+import { auth } from "@clerk/nextjs/server";
+import { isMatch } from "date-fns";
+import { redirect } from "next/navigation";
+import { NavBar } from "../_components/navbar";
+import { SummaryCards } from "./_components/summary-cards";
+import { TimeSelect } from "./_components/time-select";
+
+interface HomeProps {
+  searchParams: { month: string };
+}
+
+export default async function Home({ searchParams: { month } }: HomeProps) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/signin");
+  }
+
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+  if (monthIsInvalid) {
+    redirect(`?month=${new Date().getMonth() + 1}`);
+  }
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
+            <SummaryCards month={month} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+              {/* <TransactionsPieChart {...dashboard} /> */}
+              {/* <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              /> */}
+            </div>
+          </div>
+          {/* <LastTransactions lastTransactions={dashboard.lastTransactions} /> */}
+        </div>
+      </div>
+    </>
+  );
+}
