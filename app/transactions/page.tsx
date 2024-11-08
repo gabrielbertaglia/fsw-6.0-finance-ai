059f40e1f4fd4f12c2883d41d3488bed1c8a1f5b
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-11-08 18:48:42 -0300

    feat: ensure pages are correctly scroll

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import AddTransactionButton from "../_components/add-transaction-button";
 import { NavBar } from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
+import { ScrollArea } from "../_components/ui/scroll-area";
 import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_columns";
 
@@ -20,12 +21,14 @@ const TransactionsPage = async () => {
   return (
     <>
       <NavBar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable columns={transactionsColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={transactionsColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
   );
