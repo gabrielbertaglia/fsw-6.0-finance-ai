b9fc53f3f02636a898c9dc2e67ff642fb93d27d5
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-11-09 16:05:10 -0300

    feat: ensure only premium user can generate ai report

--- app/(home)/_components/ai-reports-button.tsx
+++ app/(home)/_components/ai-reports-button.tsx
@@ -13,16 +13,21 @@ import {
 } from "@/app/_components/ui/dialog";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import { BotIcon, Loader2Icon } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 import Markdown from "react-markdown";
 import { generateAiReport } from "../_actions/generate-ia-reports";
 // import remarkGfm from "remark-gfm";
 
 interface AiReportButtonProps {
+  hasPremiumPlan: boolean;
   month: string;
 }
 
-export const AiReportButton = ({ month }: AiReportButtonProps) => {
+export const AiReportButton = ({
+  month,
+  hasPremiumPlan,
+}: AiReportButtonProps) => {
   const [reportIsLoading, setReportIsLoading] = useState(false);
   const [report, setReport] = useState<string | null>(null);
   const handleGenerateReportClick = async () => {
@@ -49,33 +54,58 @@ export const AiReportButton = ({ month }: AiReportButtonProps) => {
           </Button>
         </DialogTrigger>
         <DialogContent className="max-w-[600px]">
-          <DialogHeader>
-            <DialogTitle>Relatório com IA</DialogTitle>
-            <DialogDescription>
-              Use inteligência artificial para gerar um relatório com insights
-              sobre suas finanças.
-            </DialogDescription>
-          </DialogHeader>
-          {report && (
-            <ScrollArea className="prose prose-slate max-h-[450px] text-white marker:text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
-              <Markdown>{report}</Markdown>
-            </ScrollArea>
+          {hasPremiumPlan ? (
+            <>
+              <DialogHeader>
+                <DialogTitle>Relatório com IA</DialogTitle>
+                <DialogDescription>
+                  Use inteligência artificial para gerar um relatório com
+                  insights sobre suas finanças.
+                </DialogDescription>
+              </DialogHeader>
+              {report && (
+                <ScrollArea className="prose prose-slate max-h-[450px] text-white marker:text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+                  <Markdown>{report}</Markdown>
+                </ScrollArea>
+              )}
+              <DialogFooter>
+                <DialogClose>
+                  <Button variant="ghost" className="font-bold">
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <Button
+                  onClick={handleGenerateReportClick}
+                  disabled={reportIsLoading}
+                  className="font-bold"
+                >
+                  {reportIsLoading && (
+                    <Loader2Icon className="mr-1 animate-spin" />
+                  )}
+                  {reportIsLoading ? "Gerando relatório..." : "Gerar relatório"}
+                </Button>
+              </DialogFooter>
+            </>
+          ) : (
+            <>
+              <DialogHeader>
+                <DialogTitle>Relatório com IA</DialogTitle>
+                <DialogDescription>
+                  Você precisa de um plano premium para gerar relatórios com IA
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose>
+                  <Button variant="ghost" className="font-bold">
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <Button asChild>
+                  <Link href="/subscription">Assianr plano premium</Link>
+                </Button>
+              </DialogFooter>
+            </>
           )}
-          <DialogFooter>
-            <DialogClose>
-              <Button variant="ghost" className="font-bold">
-                Cancelar
-              </Button>
-            </DialogClose>
-            <Button
-              onClick={handleGenerateReportClick}
-              disabled={reportIsLoading}
-              className="font-bold"
-            >
-              {reportIsLoading && <Loader2Icon className="mr-1 animate-spin" />}
-              {reportIsLoading ? "Gerando relatório..." : "Gerar relatório"}
-            </Button>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { isMatch } from "date-fns";
 import { redirect } from "next/navigation";
 import { NavBar } from "../_components/navbar";
@@ -26,6 +26,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect(`?month=${new Date().getMonth() + 1}`);
   }
   const dashboard = await getDashboard(month);
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <NavBar />
@@ -33,7 +34,12 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex gap-3">
-            <AiReportButton month={month} />
+            <AiReportButton
+              month={month}
+              hasPremiumPlan={
+                user.publicMetadata.subscriptionPlan === "premium"
+              }
+            />
             <TimeSelect />
           </div>
         </div>
